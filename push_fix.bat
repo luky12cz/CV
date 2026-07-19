6a076b9dc95de1a213049bb2b32a1c15a78310eb
LukasSkarpa12 <lukas@skarpa.dev>
2026-07-19 03:32:31 +0200

    chore: clean up repo, remove build artifacts and unused scripts

--- push_fix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\luky\Desktop\cline documents"
-"C:\Program Files\Git\bin\git.exe" add -A
-"C:\Program Files\Git\bin\git.exe" commit -m "Fix: update engineeringresumes with correct photo, email, and GitHub"
-"C:\Program Files\Git\bin\git.exe" push -u origin main
-echo Done.
-pause
